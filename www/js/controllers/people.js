6dd6f9c53ca625438286e50dcdf22fd8d8cc834e
Eric <Ericrius1@gmail.com>
2014-07-07 14:14:32 +0200

    shnurple

---
+++ www/js/controllers/people.js
@@ -0,0 +1,13 @@
+angular.module('shnurple.controllers', [])
+  .controller('PeopleCtrl', ['$scope',
+    function($scope) {
+      var people = {
+        {
+          name: "eric"
+        }, 
+        {
+          name: "robert"
+        }
+      }
+    }
+  ])
